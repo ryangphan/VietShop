82af093eb9d4382b2562ef92bc58019aef84dd75
ryangphan <thanhnhatphan@gatech.edu>
2019-04-28 15:53:18 +0700

    Initilize Customer and VietlottShop class

--- src/com/customer/Customer.java
+++ src/com/customer/Customer.java
@@ -1,4 +1,57 @@
 package com.customer;
+import java.util.*;
 
 public class Customer {
+
+    private String name;
+    private int phone;
+    private Date birthday;
+    private String address;
+    private String email;
+
+
+    public Customer(String name, int phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/com/shop/VietlottShop.java
+++ src/com/shop/VietlottShop.java
@@ -1,4 +1,75 @@
 package com.shop;
+import java.util.*;
 
 public class VietlottShop {
+
+    private String owner;
+    private int code;
+    private int phone;
+    private String address;
+    private String email;
+    private int moneyDeposit;
+    private int moneyIssue;
+
+    public VietlottShop(String owner, int code, int phone) {
+        this.owner = owner;
+        this.code = code;
+        this.phone = phone;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMoneyDeposit() {
+        return moneyDeposit;
+    }
+
+    public void setMoneyDeposit(int moneyDeposit) {
+        this.moneyDeposit = moneyDeposit;
+    }
+
+    public int getMoneyIssue() {
+        return moneyIssue;
+    }
+
+    public void setMoneyIssue(int moneyIssue) {
+        this.moneyIssue = moneyIssue;
+    }
 }
